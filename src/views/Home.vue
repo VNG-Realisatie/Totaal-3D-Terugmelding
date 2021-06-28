c077c8944426a6c4437afa89e556462360a09c14
Bart Burkhardt <bart.burkhardt@gmail.com>
2021-06-28 23:46:24 +0200

    implemented basic mvp using dummy data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,21 +13,30 @@
 
       <div class="entryform">
         <div class="formheader">Postcode</div>
+
+        <!-- <input v-model="postcode" @keypress="isNumber($event)"> -->
         <b-form-input class="forminput" v-model="postcode" ></b-form-input>   
       </div>
 
       <div class="entryform">
         <div class="formheader">Huisnummer + toevoeging</div>
-        <b-form-input class="forminput" v-model="huisnummer" disabled></b-form-input>   
+        <b-form-input class="forminput" v-model="huisnummer" v-bind:disabled="invalid_postcode"></b-form-input>   
       </div>
+
+      <div v-if="found_address" class="formlines">
+        <div>Hovevierstraat 3</div>
+        <div>BAG Id: 43248572923428</div>
+        <div>Bouwjaar: 1975</div>
+        </div>
 
     </b-col>
 
-    <b-col>
-      <div class="unity">Unity viewer here...</div>
+    <b-col>     
+        <img class="unity" v-bind:src="viewer_image" alt="">
     </b-col>
     
   </b-row>
+
 
 
 </b-container>
@@ -43,30 +52,47 @@ export default {
   data: function () {
     return {
       postcode: "",
-      huisnummer: ""
+      huisnummer: "",
+      invalid_postcode: true,
+      found_address:false,
+      viewer_default_image: "images/3dnetherlands_viewer.PNG",
+      viewer_image: ""
     }
   },
-  computed: {
-  postcode: {
-    // setter
-    set: function (newValue) {
-
-      this.postcode="";
-      // var names = newValue.split(' ')
-      // this.firstName = names[0]
-      // this.lastName = names[names.length - 1]
+  watch: {
+    postcode: function (val) {
+      //TODO add regex
+      this.invalid_postcode = val.length != 6;
+    },
+    huisnummer: function (val) {
+      //TODO check building using BAG API
+      this.found_address = val == 3;
+      if(this.found_address){
+        this.viewer_image = "images/hovenierstraat3.png";
+      }
+      else{
+        this.viewer_image = this.viewer_default_image;
+      }
     }
-  }
-},
-  // watch: {
-  //   postcode: function (newpostcode, oldpostcode) {
-  //     //console.log(newpostcode);
-  //     this.postcode = "bla";
-      
-  //     //this.answer = 'Waiting for you to stop typing...'
-  //     //this.debouncedGetAnswer()
-  //   }
-  // },
+  },
+  created:function(){
+    this.viewer_image = this.viewer_default_image;
+  },
+ methods: {
+    isNumber: function(evt) {
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) 
+      {
+        evt.preventDefault();
+      }
+      else 
+      {
+        return true;
+      }
+    }
+ },
+
   components: {
     HelloWorld
   }
@@ -103,7 +129,6 @@ export default {
 }
 
 .unity{
-  background:#bbb;
   height:400px;
 }
 
