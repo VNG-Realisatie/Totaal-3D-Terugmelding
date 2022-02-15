9e4de04193cb5b037711bbc5fd4dcc39762d9d91
Bart Burkhardt <bart.burkhardt@gmail.com>
2022-02-15 11:07:04 +0100

    aanpassingen bevindingen pagina

--- src/views/Bevindingen.vue
+++ src/views/Bevindingen.vue
@@ -5,8 +5,8 @@
     <h1>Floating point precision</h1>
   
 <p>
-Bij het maken van onze test omgeving zijn we begonnen met een 3D wereld waarbij we het middenpunt de Dom toren in Utrecht hadden gebruikt. 
-We zijn heel agile begonnen met een test adres in Utrecht, Stadhouderslaan 79. 
+Bij het maken van onze testomgeving zijn we begonnen met een 3D wereld waarbij we de Dom toren in Utrecht als middenpunt hadden gebruikt. 
+We zijn begonnen te ontwikkelen met een test adres in Utrecht, Stadhouderslaan 79. 
 Na presentatie van de sprint resultaten in de tweewekelijks sprint review kregen we vaak als feedback de vraag of we ook andere steden beschikbaar zouden kunnen stellen. 
 We hebben toen in de sprint rond de jaarwisseling opgenomen dat we alle gebouwen van Nederland zouden genereren.
 Dit is inmiddels voltooid en nu kan elk adres ingevoerd worden.
@@ -14,22 +14,22 @@ Dit is inmiddels voltooid en nu kan elk adres ingevoerd worden.
 
 <p>
 Dit resulteerde in veel nieuwe feedback van meerdere adressen waar niet alles lekker in loopt. 
-Een van de problemen is dat bij adressen ver weg van het middenpunt de rendering wat gaat bibberen. 
+Een van de problemen is dat bij adressen die verder van het middenpunt zijn de rendering wat gaat bibberen. 
 Dit heeft te maken met <b>floating point precision</b>
 </p>
 
 <p>
-Een float is een getal met precisie achter de komma. Unity gebruikt floats voor de positie van 3D objecten. Elke meter is een eenheid. 
-Dus een Unity positie 10.1 staat dan op 10 meter en 10 centimer. Een float is verdeeld in eenheden (meter) en de precisie (centimers en milimeters)
-Als je dus een positie heb met een groot aantal eenheden heb je minder precisie. Unity zal bij berekingen dan gaan afronden in de precisie die nog over is en dit resulteerd dan
-in een bibberend beeld vooral als je gaat animeren.
+Een float is een getal met een precisie achter de komma. Unity gebruikt floats voor de positie van 3D objecten. In onze 3D omgeving is elke meter is een eenheid dus het getal voor de komma.
+Dus een object met positie 10.1 staat dan op 10 meter en 10 centimeter. Een float is verdeeld in eenheden (meter) en de precisie (centimers en milimeters)
+Als je dus een positie heb met een groot aantal eenheden heb je minder precisie. De applicatie zal bij berekingen dan gaan afronden in de precisie die nog over is en dit resulteert dan
+in een bibberend beeld vooral als je de camera of objecten gaat verplaatsen.
 </p>
 
 <p>
 Na ongeveer 5000 meter gaat dit problemen geven.
 In onderstaand voorbeeld zie je een adres in Hengelo. 
-Zoals je kan zien gaat de positie ver over de limiet en gaat er van alles mis bij in dit geval het roteren van de camera.
-Unity geeft het het zelfs al aan dat een 3D object te ver weg van middenpunt staat, en dit moet dus opgelost worden.
+Zoals je kan zien gaat de positie ver over de limiet en ontstaat er een bibberend beeld.
+Unity, onze ontwikkeltool, geeft het het zelfs al aan dat een 3D object te ver weg van middenpunt staat, en dit moet dus opgelost worden.
 </p>
 
 <p>
@@ -43,7 +43,7 @@ Unity geeft het het zelfs al aan dat een 3D object te ver weg van middenpunt sta
 <h2>Oplossing</h2>
 
 <p>
-Een oplossing is op het middepunt punt te verplaatsen naar het middenpunt van het opgevraagde adres    
+Een oplossing is om het middepunt te verplaatsen naar de positie van het opgevraagde adres.
 </p>
 
   
