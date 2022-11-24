eb461e4d3adfe90e24fda81f796ad30b37258927
Tom Simons <tom.v.simons@gmail.com>
2022-11-24 15:32:35 +0100

    removed comments

--- UNITY/Assets/T3D/Scripts/Uitbouw/Base/ShapableUitbouwCityObject.cs
+++ UNITY/Assets/T3D/Scripts/Uitbouw/Base/ShapableUitbouwCityObject.cs
@@ -18,34 +18,11 @@ namespace T3D.Uitbouw
 
         protected void Awake()
         {
-            //base.Start();
             var building = GetComponent<UitbouwBase>().ActiveBuilding;
             Initialize(building.MainCityObject);
             print(building.MainCityObject);
-            //Debug.LogError("set parents here after finishing converting to T3DPipeline");
-            //SetParents(new CityObject[] {
-            //    building.MainCityObject
-            //});
         }
 
-        //todo
-        //public override CitySurface[] GetSurfaces()
-        //{
-        //    List<CitySurface> citySurfaces = new List<CitySurface>();
-        //    var walls = GetComponentsInChildren<UitbouwMuur>();
-        //    foreach (var wall in walls)
-        //    {
-        //        citySurfaces.Add(wall.Surface);
-        //    }
-
-        //    var boundaryFeatures = GetComponentsInChildren<BoundaryFeature>();
-        //    foreach (var bf in boundaryFeatures)
-        //    {
-        //        citySurfaces.Add(bf.Surface);
-        //    }
-
-        //    return citySurfaces.ToArray();
-        //}
 
         public void Initialize(CityObject parent)
         {
