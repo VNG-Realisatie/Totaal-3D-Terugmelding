9cc12bc6a3ba2f25a5e4677f8d86fef4a2753d59
Tom Simons <tom.v.simons@gmail.com>
2022-12-19 15:44:17 +0100

    added new texts and changed layout

--- WEB/src/views/Home.vue
+++ WEB/src/views/Home.vue
@@ -14,23 +14,49 @@ Deze projectpagina geeft toegang naar de ontwikkelde proefopstellingen en de doc
 
   <b-row class="buttons">    
       <b-col class="linkcard">
-        <h2>Terugmelden</h2>
-        <div>Klopt er iets niet aan de 3D BAG? Geef dit aan en doe een mutatieverzoek.</div>
+        <h2>Terugmeldtool</h2>
+        <div>
+          Deze technische demo is een beproeving van een keten van verschillende features ontwikkeld in Unity, welke mogelijk ingezet kunnen worden voor een 3D-terugmeldtool. Het concept van de ontwikkelde features is m.b.v. een prototype gevalideerd met ambtenaren van Gemeente Amsterdam wie betrokken zijn bij 2D terugmeldingen en basisregistraties.
+Het eindresultaat is een haalbaarheidsstudie, met de nadruk op de techniek en de output: een CityJSON bestand welke óók geschikt is bevonden om te kunnen inwinnen en te registreren.
+        </div>
         <!-- <b-button href="#/terugmelden" class="button-link">Start Terugmelden</b-button> -->
         <b-button @click="OpenSession()" class="button-link">Start Terugmelden</b-button>
       </b-col>
 
-      <b-col class="linkcard">
-        <h2>Vergunnings-checker</h2>
-        <div>Benieuwd naar wat er vergunningsvrij aangebouwd mag worden?</div>
-        <b-button href="/vergunningschecker#/vergunningschecker" class="button-link">Start Vergunningscheck</b-button>  
+
+  </b-row>
+  <b-row style="background-color: #F5F5F5;">
+    <b-col class="linkcard">
+        <h2>Vergunningschecker</h2>
+        <div>
+          Het doel van de proefopstelling was tweeledig. Het eerste doel was de (technische) haalbaarheid te onderzoeken van een te ontwikkelen vergunningschecker en een tekentool in een Unity viewer (spoor gebruik).
+Het tweede doel was het onderzoeken of een 3D model mogelijk te verkrijgen uit een vergunnings- proces, gebruikt kan worden om daarmee de landelijke 3D-database van de proefopstelling bij te werken (inwinnen). De gebruiker kan in deze demo een aan- of uitbouw tegen vijf checks uit de omgevingswet valideren en zien of deze vergunningsvrij gebouwd mag worden. De bevindingen en conclusies zijn te lezen in het verslag op de <a href="https://alkem.io/t3d/dashboard">projectwebsite</a>.
+</div>
       </b-col>
 
       <b-col class="linkcard">
         <h2>Gemeenteblad</h2>
-        <div>Bekijk het bouwplan van de buren op een 3D kaart en ervaar de impact.</div>
-        <b-button href="#/gemeenteblad" class="button-link">Open gemeenteblad</b-button>
+        <div>Elke gemeente publiceert door hen verleende vergunningen, zodat burgers en belanghebbenden in de gelegenheid gesteld worden bezwaar te maken. In de bekendmaking is te lezen op welk adres er welke (bouw)vergunningen zijn afgegeven. Mochten bewoners inzage willen hebben in bijvoorbeeld bouwtekeningen, dan kunnen zij hiervoor een afspraak moeten maken om deze in te zien bij het Gemeenteloket.
+De proefopstelling had als doel te valideren wat gebruikers vinden van een bekendmaking voorzien van een 3D-model. De bevindingen en conclusies zijn te lezen in het verslag op de <a href="https://alkem.io/t3d/dashboard">projectwebsite</a>. 
+</div>
+
       </b-col>
+  </b-row>
+  <b-row style="background-color: #F5F5F5;">
+    <b-col class="linkcard" style="padding-top: 0;">        <b-button href="/vergunningschecker#/vergunningschecker" class="button-link">Start Vergunningschecker</b-button>  </b-col>
+    <b-col class="linkcard" style="padding-top: 0;">        <b-button href="#/gemeenteblad" class="button-link">Open gemeenteblad</b-button></b-col>
+  </b-row>
+  <b-row>
+    <b-col class="linkcard">
+        <h2>Binnenkanten</h2>
+        <div>Amsterdam heeft ook gewerkt aan het verbeteren van een algoritme dat automatisch een 3D puntenwolk van de binnenkant van een pand omzet naar een 3D model, welke omgezet kan worden naar een CityJSON bestand. 
+Deze binnenzijde kan dan middels de terugmeldtool, ontwikkeld in Amsterdam, worden gepositioneerd op de juiste positie binnen de buiten-geometrie van het pand. Het registreren van binnen-geometrie is in CityJSON 1.0 nog niet mogelijk.
+Dit datamodel staat niet toe dat een interne toevoeging aan de geometrie van een kamer (BuildingRoom) in de hiërarchie een afgeleide van het hoofdgebouw (Building) kan zijn. Om het in deze keten tóch mogelijk te maken om óók de binnenkant te kunnen registreren in CityJSON 1.0, is er daarom voor gekozen de kamers als BuildingParts (externe toevoeging) te vermelden in het informatiemodel. De bevindingen en conclusies zijn te lezen in het verslag op de <a href="https://alkem.io/t3d/dashboard">projectwebsite</a>.
+</div>
+      </b-col>
+
+      <b-col class="linkcard">
+        <div class="main-image-container"><img src="/images/binnenkanten.png" class="main-image"></div>      </b-col>
   </b-row>
 
 
@@ -162,7 +188,7 @@ p{
   background-repeat:no-repeat;
   background-size:cover;
   padding-top: 40px;
-  padding-bottom: 10px;
+  padding-bottom: 40px;
   margin-right: -15px;
   margin-left: -15px;
   background-color: #00A03C;
@@ -171,7 +197,7 @@ p{
 .main-image-container{
   margin-left: 50px;
   margin-right: 50px;
-  margin-bottom: 30px;
+  /* margin-bottom: 30px; */
   overflow: hidden;
 }
 .main-image{
@@ -180,17 +206,20 @@ p{
 }
 
 .buttons{
- background-color: #ff9100;
+ /* background-color: #ff9100; */
  padding: 40px; 
 }
 
 .linkcard{
-  background-color: #fff;
+  /* background-color: #fff; */
   font-size: 16px;
   width: 50%;
+  max-width: 50%;
   padding: 20px;
-  margin-right:10px;
-  margin-left:10px;  
+  margin-left: auto;
+  margin-right: auto;
+  /* margin-right:10px; */
+  /* margin-left:10px;   */
 }
 
 .avatar{
@@ -215,7 +244,8 @@ p{
 
 
 .button-link{
-  margin-top:40px;
+  margin-top:20px;
+  margin-bottom: 20px;
   background-color: #01489A;
 }
 
